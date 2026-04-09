c703aab88c3a58abc4f67c5b395a8d30d24ad7be
Bon <bichbon1508@gmail.com>
2026-04-09 19:55:14 +0700

    Kết nối database máy cá nhân

--- Config/db.php
+++ Config/db.php
@@ -1,8 +1,8 @@
 <?php
 $host="localhost";
 $name="root";
-$password="Quynh3112";
-$db="ql_rapchieu";
+$password="";
+$db="ql_rap";
 $conn=new mysqli($host,$name,$password,$db);
 if($conn->connect_error){
     die("Kết nối thất bại");
